b1092a2fbde2d4d1be05348b905d4ca1244e302f
William Bout <hello@williambout.me>
2019-01-09 22:45:42 -0800

    :construction:

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import Parallax from "scroll-parallax";
+import anime from "animejs/lib/anime.es.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   const p = new Parallax(".parallax").init();
+
+  anime({
+    targets: ".heading",
+    translateY: [250, 0],
+    opacity: 1,
+    easing: "easeInOutQuart",
+    delay: anime.stagger(150),
+    duration: 1100
+  });
 });
