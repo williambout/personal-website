660e236abfcfd2edc77deca196d1ceb6c510daaf
William Bout <hello@williambout.me>
2019-02-23 09:14:40 -0800

    :construction:

--- dist/personal-website.e31bb0bc.js
+++ dist/personal-website.e31bb0bc.js
@@ -1947,7 +1947,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49860" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50017" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
