366a2ffef19cad850ecce1fc497380b937abc188
William Bout <hello@williambout.me>
2019-06-29 17:37:09 -0700

    Optimized images

--- dist/main.c1d0e28d.js
+++ dist/main.c1d0e28d.js
@@ -243,7 +243,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49465" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64867" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
