730ff19a11ac5b54730ac4b745c5cf267566c0db
William Bout <hello@williambout.me>
2019-11-27 18:32:25 -0800

    Nav wip

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -14,4 +14,40 @@ document.addEventListener("DOMContentLoaded", () => {
     const scrollLeft = event.target.scrollLeft;
     photographyFade.seek(photographyFade.duration * (scrollLeft / 75));
   });
+
+  const navFade = anime({
+    targets: "nav",
+    opacity: [0, 1],
+    easing: "linear",
+    autoplay: false
+  });
+
+  const headerHeight = document.querySelector("header").clientHeight;
+
+  window.addEventListener("scroll", event => {
+    const scrollTop = window.scrollY;
+
+    navFade.seek(
+      navFade.duration * (Math.max(0, scrollTop - headerHeight + 100) / 50)
+    );
+  });
+
+  const scrollElement =
+    window.document.scrollingElement ||
+    window.document.body ||
+    window.document.documentElement;
+  const workLink = document.querySelector(".button-work");
+  const workTop = document.querySelector("#work").getBoundingClientRect().top;
+
+  workLink.addEventListener("click", () => {
+    anime({
+      targets: scrollElement,
+      scrollTop: workTop,
+      duration: 500,
+      easing: "spring(1, 100, 10, 0)",
+      complete: function(anim) {
+        workLink.classlist.add("active");
+      }
+    });
+  });
 });
