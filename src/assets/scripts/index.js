ee593d559bf7fa88aaece3752a21e698fb8a4970
William Bout <hello@williambout.me>
2021-06-11 08:38:23 -0700

    Remove lazyload

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,101 +1,106 @@
-import anime from 'animejs/lib/anime.es.js';
-import LazyLoad from 'vanilla-lazyload';
-import smoothscroll from 'smoothscroll-polyfill';
+import anime from "animejs/lib/anime.es.js";
+import smoothscroll from "smoothscroll-polyfill";
 
 smoothscroll.polyfill();
 
-document.addEventListener('DOMContentLoaded', () => {
-	const lazyLoadInstance = new LazyLoad({
-		elements_selector: '.lazy',
-	});
-
+document.addEventListener("DOMContentLoaded", () => {
 	const links = document.querySelectorAll('a[href^="#"]');
-	links.forEach(link => {
-		const id = link.attributes['href'].value.slice(1);
+	links.forEach((link) => {
+		const id = link.attributes["href"].value.slice(1);
 		const target = document.getElementById(id);
-		link.addEventListener('click', () => {
-			target.scrollIntoView({ behavior: 'smooth' });
+		link.addEventListener("click", () => {
+			target.scrollIntoView({ behavior: "smooth" });
 		});
 	});
 
-	const photoList = document.querySelector('.photolist');
+	const photoList = document.querySelector(".photolist");
 
 	const photographyFade = anime({
-		targets: '#photography-title',
+		targets: "#photography-title",
 		opacity: 0.15,
-		easing: 'linear',
+		easing: "linear",
 		autoplay: false,
 	});
 
 	let canScrollLeft = false;
 	let canScrollRight = true;
 
-	photoList.addEventListener('scroll', event => {
+	photoList.addEventListener("scroll", (event) => {
 		const scrollLeft = event.target.scrollLeft;
 
 		photographyFade.seek(photographyFade.duration * (scrollLeft / 75));
 
-		if (photoList.scrollWidth === photoList.clientWidth + photoList.scrollLeft) {
-			photoScrollRight.classList.remove('active');
+		if (
+			photoList.scrollWidth ===
+			photoList.clientWidth + photoList.scrollLeft
+		) {
+			photoScrollRight.classList.remove("active");
 			canScrollRight = false;
 		}
 
-		if (!canScrollRight && photoList.scrollWidth > photoList.clientWidth + photoList.scrollLeft) {
-			photoScrollRight.classList.add('active');
+		if (
+			!canScrollRight &&
+			photoList.scrollWidth > photoList.clientWidth + photoList.scrollLeft
+		) {
+			photoScrollRight.classList.add("active");
 			canScrollRight = true;
 		}
 
 		if (scrollLeft > 0 && !canScrollLeft) {
-			photoScrollLeft.classList.add('active');
+			photoScrollLeft.classList.add("active");
 			canScrollLeft = true;
 		}
 
 		if (scrollLeft === 0 && canScrollLeft) {
-			photoScrollLeft.classList.remove('active');
+			photoScrollLeft.classList.remove("active");
 			canScrollLeft = false;
 		}
 	});
 
-	const photoScrollRight = document.querySelector('.scroll-right');
-	const photoScrollLeft = document.querySelector('.scroll-left');
+	const photoScrollRight = document.querySelector(".scroll-right");
+	const photoScrollLeft = document.querySelector(".scroll-left");
 
-	photoScrollRight.addEventListener('click', event => {
+	photoScrollRight.addEventListener("click", (event) => {
 		photoList.scrollTo({
 			top: 0,
 			left: photoList.scrollLeft + 400,
-			behavior: 'smooth',
+			behavior: "smooth",
 		});
 	});
 
-	photoScrollLeft.addEventListener('click', event => {
+	photoScrollLeft.addEventListener("click", (event) => {
 		photoList.scrollTo({
 			top: 0,
 			left: photoList.scrollLeft - 400,
-			behavior: 'smooth',
+			behavior: "smooth",
 		});
 	});
 
 	const navFade = anime({
-		targets: 'nav',
+		targets: "nav",
 		opacity: [0, 1],
-		easing: 'linear',
+		easing: "linear",
 		autoplay: false,
 	});
 
-	const headerHeight = document.querySelector('header').clientHeight;
+	const headerHeight = document.querySelector("header").clientHeight;
 
-	window.addEventListener('scroll', event => {
+	window.addEventListener("scroll", (event) => {
 		const scrollTop = window.scrollY;
 
-		navFade.seek(navFade.duration * (Math.max(0, scrollTop - headerHeight + 100) / 50));
+		navFade.seek(
+			navFade.duration * (Math.max(0, scrollTop - headerHeight + 100) / 50)
+		);
 	});
 
 	const changeNav = (entries, observer) => {
-		entries.forEach(entry => {
+		entries.forEach((entry) => {
 			if (entry.isIntersecting && entry.intersectionRatio >= 0.2) {
-				document.querySelector('.active').classList.remove('active');
-				var id = entry.target.getAttribute('id');
-				var newLink = document.querySelector(`[href="#${id}"]`).classList.add('active');
+				document.querySelector(".active").classList.remove("active");
+				var id = entry.target.getAttribute("id");
+				var newLink = document
+					.querySelector(`[href="#${id}"]`)
+					.classList.add("active");
 			}
 		});
 	};
@@ -106,13 +111,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	const observer = new IntersectionObserver(changeNav, options);
 
-	const sections = document.querySelectorAll('section');
-	sections.forEach(section => {
+	const sections = document.querySelectorAll("section");
+	sections.forEach((section) => {
 		observer.observe(section);
 	});
-});
-
-window.addEventListener('load', () => {
-	const avatar = document.querySelector('.avatar');
-	console.log('loaded');
 });
