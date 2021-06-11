a60895dbb44326e3fc5443253abc780d37b2034f
William Bout <hello@williambout.me>
2021-06-11 08:50:37 -0700

    Update project.js

--- src/assets/scripts/project.js
+++ src/assets/scripts/project.js
@@ -1,9 +1,5 @@
-import LazyLoad from 'vanilla-lazyload';
-import mediumZoom from 'medium-zoom';
+import mediumZoom from "medium-zoom";
 
-document.addEventListener('DOMContentLoaded', () => {
-	mediumZoom('[data-zoomable]', { margin: 24, background: '#1d1d1d' });
-	const lazyLoadInstance = new LazyLoad({
-		elements_selector: '.lazy',
-	});
+document.addEventListener("DOMContentLoaded", () => {
+	mediumZoom("[data-zoomable]", { margin: 24, background: "#1d1d1d" });
 });
